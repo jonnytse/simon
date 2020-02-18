ee07da8fe314015bd6400de201dbd0a86c8289ef
Jonny Tse <Jonny.Tse@Nology.local>
2020-02-18 08:44:09 +0000

    latest commit as of 18/02/2020 morning

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const onload = () =>  {
 console.log('hello');
 
+let randomNum = (Math.floor(Math.random()*4));
+console.log(randomNum);
+
 playerMovesArray = [];
 computerMovesArray = [];
+
+// for (i = 0; i < 50; i++) {
+//     return (Math.floor(Math.random()*4)+1);
+//     computerMovesArray.push(randomNum);
+//     console.log(computerMovesArray);
+// }
 
 const greenButton = $("#greenQuad");
 const redButton = $("#redQuad");
 const blueButton = $("#blueQuad");
 const yellowButton = $("#yellowQuad");
 
-console.log($("div").index(greenButton));
-console.log($("div").index(redButton));
-console.log($("div").index(yellowButton));
-console.log($("div").index(blueButton));
-
-
+console.log($(".box").index(greenButton));
+console.log($(".box").index(redButton));
+console.log($(".box").index(yellowButton));
+console.log($(".box").index(blueButton));
 
 $(".box").click(function() {
-    $(this).index(".box");
-    console.log(this);
+    // console.log($(this).index(".box")); //this console.logs index
+    playerMovesArray.push($(".box").index(this));
+    console.log(playerMovesArray);
 })
 
 }; //const onload closing curly braces
