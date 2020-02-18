9064c3c61e304d431f3874ff239975a866687732
Jonny Tse <Jonny.Tse@Nology.local>
2020-02-18 22:12:45 +0000

    added start/reset button, which populates an array of 50x computer moves

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const onload = () =>  {
 console.log('hello');
 
-clicked = true;
-
-let randomNum = (Math.floor(Math.random()*4));
-console.log(randomNum);
-
-playerMovesArray = [];
-computerMovesArray = [];
+let computerMovesArray = [];
+let playerMovesArray = [];
+let playerMove = 0
 
 // for (i = 0; i < 50; i++) {
 //     return (Math.floor(Math.random()*4)+1);
@@ -15,10 +11,12 @@ computerMovesArray = [];
 //     console.log(computerMovesArray);
 // }
 
+const startButton = $("#start-reset");
 const greenButton = $("#greenQuad");
 const redButton = $("#redQuad");
 const blueButton = $("#blueQuad");
 const yellowButton = $("#yellowQuad");
+const turnCount = $(".count")
 
 
 console.log($(".box").index(greenButton));
@@ -26,11 +24,25 @@ console.log($(".box").index(redButton));
 console.log($(".box").index(yellowButton));
 console.log($(".box").index(blueButton));
 
+
+$(startButton).click(function() {
+    win = false;
+    computerMovesArray = [];
+    playerMovesArray = [];
+    playerMove = 0;
+    for (i = 0; i < 50; i++) {
+        computerMovesArray.push((Math.floor(Math.random()*4)));
+    }
+    console.log(computerMovesArray);
+})
+
 $(".box").click(function() {
     // console.log($(this).index(".box")); //this console.logs index
     playerMovesArray.push($(".box").index(this));
     console.log(playerMovesArray);
 })
+
+
 
 // $(".box").click(function() {
 //    let yellowButtonColor = clicked? 'lightyellow' : 'yellow'
@@ -38,31 +50,39 @@ $(".box").click(function() {
 //    clicked = !clicked;
 // });
 
+// $(".box").click(function() {
+//     if ($(this).index(".box") === 0) {
+//         let color = clicked? 'lightgreen' : 'green';
+//         $(this).css('background-color', color);
+//         clicked = !clicked;
+
+//     } else if ($(this).index(".box") === 1) {
+//         let color = clicked? 'pink' : 'red';
+//         $(this).css('background-color', color);
+//         clicked = !clicked;
+
+//     }  else if ($(this).index(".box") === 2) {
+//         let color = clicked? 'lightyellow' : 'yellow';
+//         $(this).css('background-color', color);
+//         clicked = !clicked;
+
+//     } else if ($(this).index(".box") === 3) {
+//         let color = clicked? 'lightblue' : 'blue';
+//         $(this).css('background-color', color);
+//         clicked = !clicked;
+//     } 
+// })
+
 $(".box").click(function() {
-    if ($(this).index(".box") === 0) {
-        let color = clicked? 'lightgreen' : 'green';
-        $(this).css('background-color', color);
-        clicked = !clicked;
-
-    } else if ($(this).index(".box") === 1) {
-        let color = clicked? 'pink' : 'red';
-        $(this).css('background-color', color);
-        clicked = !clicked;
-
-    }  else if ($(this).index(".box") === 2) {
-        let color = clicked? 'lightyellow' : 'yellow';
-        $(this).css('background-color', color);
-        clicked = !clicked;
-
-    } else if ($(this).index(".box") === 3) {
-        let color = clicked? 'lightblue' : 'blue';
-        $(this).css('background-color', color);
-        clicked = !clicked;
-    } 
-})
-
+   $(this).animate ({opacity: 0.3}, 500, function() {
+   });     
+}); 
 
 
 
 }; //const onload closing curly braces
 $(onload);
+
+// set time out 500
+// opacity
+// click
